5b9b3b96dbfa36a9f708d5bb8082dd2557bb6d96
ghelms <gustav.helms@yahoo.dk>
2021-01-07 19:29:27 +0100

    g

--- Class 2 exercise.R
+++ Class 2 exercise.R
@@ -1,3 +1,9 @@
+#Install packages
+install.packages("pacman")
+library(pacman)
+
+pacman::p_load(tidyverse)
+
 #Exercise 1
 # 1 students with 39 shoesize or bigger
 shoe39big <- filter(df,df$shoesize>=39)
@@ -30,7 +36,7 @@ all.of.the.above <- filter(df,
 # 1 Sort data so it starts with the student who was the slowest at tongue twister
 descending.tongue.twist <- arrange(df,(df$tongue_twist))
 
-# 2 Sort data by one argument to find the student who performed best in the Romberg task. You define ”best”. 
+# 2 Sort data by one argument to find the student who performed best in the Romberg task. You define ”best”
 Best.roomberg <- arrange(df,desc(df$romberg_closed),desc(df$shoesize))
 #When entering more than one collumn to arrange the next collum (in this example shoesize) will break 
 #the ties from the first arranged collumn (romberg_closed)
@@ -63,20 +69,40 @@ vars.df <- select(df,vars)
 gender.shoesize.df <- select(df,gender,shoesize,everything())
 
 #The "Mutate"-function e.g.
-mutate(df,breath.hold.min=df$breath_hold/60)
+df <- mutate(df,breath.hold.min=df$breath_hold/60)
 #Here the collumn breath.hold.min is added to the df, and collumn contain the breath.hold in minutes
 
 #Exercise 4
 #1 The tongue twister had 99 words. Make a new column called ”words_per_sec” where you calculate how 
 #many words each student said per second. 
-mutate.df <- mutate(df,words.per.sec=(60/df$tongue_twist)*99) 
-select(mutate.df,name,tongue_twist,words.per.sec)
-#Here i am selecting the tree relevant collums to check if the formular was correct
-mutate.df <- format(mutate.df,digits=0)
+df <- mutate(df,words.per.sec=(60/df$tongue_twist)*99) 
+df$words.per.sec <- format(mutate.df$words.per.sec , digits=0)
 #Here i set the digits in the collumn to 0
 
-#2
+#2 Currently breath_hold is in seconds. Convert it to two new columns called ”breath_min” and 
+#”breath_sec”, containing the number of whole minutes (achieved by dividing using %/%) 
+#and remaining seconds respectively. 
+df <- mutate(df,breath_min=(df$breath_hold%/%60))
+df <- mutate(df,breath_sec=(df$breath_hold-(60*df$breath_min)))
+df$breath_sec <- format(df$breath_sec,digits = 0)
 
+#3 Create a new column where you calculate how far each student is from the average words per second.
+df$words.per.sec <- as.numeric(df$words.per.sec)
+df <- mutate(df,deviance_words_mean=(df$words.per.sec-mean(df$words.per.sec)))
+df$deviance_words_mean <- format(df$deviance_words_mean,digits = 0)
 
+#Excercise 5
+#1 Is there a gender difference when it comes to balloon balancing?
+df %>% group_by(gender) %>% summarise(mean(balloon_balance))
+
+#2 Is there a relation between sound level preference and which cola was chosen?
+df %>% group_by(taste_cola) %>% summarise(mean(sound_level_pref))
+
+#3 Does handedness influence tongue twisting speed?
+df %>% group_by(df$handedness) %>% summarise(mean(tongue_twist))
+
+df %>% group_by(df$handedness) %>% summarise(mean(tongue_twist),length(handedness))
+
+df %>% group_by(df$handedness) %>% summarise(mean(tongue_twist),n())
 
 
